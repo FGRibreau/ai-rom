4293533fa5e445bdc855423f4b3be1b4092c50dc
Mana <mana@mana-Ubuntu.(none)>
2010-10-29 10:17:33 +0200

    commit philou

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include "City.h"
+typedef enum{false,true}bool;
 
 int main (int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+	 	
+	/*---------INITIALISATION----------*/
+	pCity Arad = cityCreate("Arad");
+	pCity Zerind = cityCreate("Zerind");
+	pCity Oradea = cityCreate("Oradea");
+	pCity Siblu = cityCreate("Siblu");
+	pCity Timisoara = cityCreate("Timisoara");-
+	pCity Lugoj = cityCreate("Lugoj");
+	pCity Mehadia = cityCreate("Mehadia");
+	pCity Dobreta = cityCreate("Dobreta");
+	pCity Craiova = cityCreate("Craiova");
+	pCity Pitesti = cityCreate("Pitesti");
+	pCity Fagaras = cityCreate("Fagaras");
+	pCity Bucarest = cityCreate("Bucarest");
+	/*-------------------------------------*/
+	
+	/*--------------DESTRUCTION------------*/
+		
+	
+	return 0;
 }
