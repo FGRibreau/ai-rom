97c8bc440ae14b0e63ac1d38ebba09f85df241fa
FG Ribreau <github@fgribreau.com>
2010-11-16 16:02:40 +0100

    Suppression des commentaires

--- src/Search/SearchAStar.c
+++ src/Search/SearchAStar.c
@@ -40,38 +40,3 @@ pFileData_city SearchAStar(pCity from, pCity to){
 	
 	return bestRoad;
 }
-
-//Release all "FileData_city_append"
-/*
- pFileData_city road = FileData_city_create(NULL, from, 0, 0); // Pas forcement optimal
- 
- pFileData_city curCity = road;
- 
- do {	
- 
- pBranch neighbour = curCity->city->branch
- ,		bestCity = neighbour;//Ville voisine
- 
- float	distBest = 99999999999
- ,		distToDest = 0;
- 
- //printf("[ ");
- do {//Parcours les enfants de curCity->city
- 
- distToDest = City_distBtw(neighbour->city, to);
- 
- if((distToDest + neighbour->dist) < distBest
- && !FileData_cityAlreadyExist(road,neighbour->city)){
- bestCity = neighbour;
- distBest = distToDest+ neighbour->dist;
- }
- 
- 
- //printf("%s %.4f\t\t", neighbour->city->name, distToDest+ neighbour->dist);
- 
- } while (neighbour = neighbour->next);//Parcour la liste chainée des villes voisines de City
- //printf("]\n");
- curCity = road = FileData_city_append(road, bestCity);
- 
- } while (curCity->city != to);
- */
